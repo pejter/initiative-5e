3275f871088c7ae9f735c04c3f3ed2a95244dfb2
Pejter <piotrluczak1995@gmail.com>
2016-05-10 21:11:31 +0200

    Added service worker for offline support

---
+++ offline.js
@@ -0,0 +1,32 @@
+// Version 0.1
+
+var cacheName = 'initiative-5e-ver-1';
+var filesToCache = [
+	'index.html'
+];
+
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(cacheName)
+		.then(function(cache) {
+			return cache.addAll(filesToCache);
+		})
+		.then(function() {
+			return self.skipWaiting();
+		});
+	);
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
